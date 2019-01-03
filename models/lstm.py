fc98a4893169396a0ca7ebc5d808ea9f6627113e
qcymkxyc <qcymkxyc@163.com>
2019-01-03 15:36:32 +0800

    建立LSTM模型

--- models/lstm.py
+++ models/lstm.py
@@ -1,17 +1,27 @@
-#!usr/bin/env python 
-# -*- coding:utf-8 _*-  
-""" 
+#!usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
  @author:qcymkxyc
  @email:qcymkxyc@163.com
  @software: PyCharm
  @file: lstm.py
  @time: 2019/1/1 21:32
- 
+
 """
+from tensorflow import keras
 import tensorflow as tf
-import keras
 
-def create_network():
-    """
 
+def create_lstm(vocab_size):
+    """建立LSTM网络
+
+    :param vocab_size: int
+        词汇表的个数
     """
+    model = keras.Sequential()
+    model.add(keras.layers.Embedding(vocab_size, 200))
+    model.add(keras.layers.GlobalAveragePooling1D())
+    model.add(keras.layers.Dense(units=80, activation=tf.nn.relu))
+    model.add(keras.layers.Dense(units=1, activation=tf.nn.sigmoid()))
+
+    return model
