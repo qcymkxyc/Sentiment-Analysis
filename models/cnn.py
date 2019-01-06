b3e5843bbb388485c1fe11ac66496c44469ace0f
qcymkxyc <qcymkxyc@163.com>
2019-01-06 19:31:03 +0800

    完成CNN

--- models/cnn.py
+++ models/cnn.py
@@ -1,15 +1,19 @@
-#!usr/bin/env python 
-# -*- coding:utf-8 _*-  
-""" 
+#!usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
  @author:qcymkxyc
  @email:qcymkxyc@163.com
  @software: PyCharm
  @file: cnn.py
  @time: 2019/1/1 21:33
- 
+
 """
 from tensorflow import keras
 import tensorflow as tf
+
+conv_size = 5
+filters = 4
+hidden_units = [80, ]
 
 
 def create_cnn(vocab_size):
@@ -20,5 +24,15 @@ def create_cnn(vocab_size):
     :return: model
     """
     model = keras.Sequential()
-    model.add(keras.layers.Embedding(input_dim=vocab_size,output_dim=200))
-    model.add(keras.layers.Conv)
+    model.add(keras.layers.Embedding(input_dim=vocab_size, output_dim=200))
+    model.add(keras.layers.Conv1D(
+        filters=filters,
+        kernel_size=conv_size,
+        activation=tf.nn.tanh)
+    )
+    model.add(keras.layers.GlobalMaxPool1D())
+    for i, unit in enumerate(hidden_units):
+        model.add(keras.layers.Dense(units=unit,activation=tf.nn.relu))
+    model.add(keras.layers.Dense(units=1, activation=tf.nn.sigmoid))
+
+    return model
